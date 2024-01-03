87fc862fe475d5d241524e2e44a8b5c3e5f1955f
malo.girard <malo.girard@jellysmack.com>
2024-01-03 17:34:28 +0100

    chore: 🚧 wip `docker-compose-production.yaml` file

--- bksys_account_ms/bksys_account_ms/settings.py
+++ bksys_account_ms/bksys_account_ms/settings.py
@@ -13,7 +13,7 @@ class AppSettings(BaseSettings):
     DB_PASSWORD: str
     DB_NAME: str
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", secrets_dir="/run/secrets")
 
 
 app_settings = AppSettings()
